82eae4fc37e66b7cb75ec1c24b16f80e7d4da3d5
Gabriel Nakamoto <gabriel@nakamoto.ca>
2025-02-02 12:55:33 -0500

    Rewrite, removed all classes, replaced select() sys call with poll()

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
-#include <cstring>
+#include <string>
 
-// #include "node.h"
-#include "daemon.h"
-#include "socket.h"
+#include "p2p.h"
 
 /*
  * 	Where do sockets come into play in a P2P network?
@@ -25,68 +23,16 @@
  */
 
 
-void server()
-{
-	Socket::Address serverAddress(INADDR_ANY, -1);
-
-	Socket handlerSocket(serverAddress);
-
-	handlerSocket.Bind();
-
-	std::cout << "Connected to port " << handlerSocket.getPort() << std::endl;
-
-	handlerSocket.Listen(1);
-
-	while (true)
-	{
-		std::unique_ptr<Socket> connectionSocket = handlerSocket.Accept();	
-
-		std::cout << "Accepted client connection\n";
-
-		const std::string message = connectionSocket->Recv();
-		std::cout << "\"" << message << "\"\n";
-	}
-}
-
-void client(int port)
-{
-	try
-	{
-		Socket::Address serverAddress(INADDR_ANY, port);
-
-		Socket connectionSocket(serverAddress);
-
-		connectionSocket.Connect();
-
-		std::cout << "Connected to server\n";
-
-		std::cout << "Message: ";
-
-		std::string message;
-
-		std::cin >> message;
-
-		connectionSocket.Send(message);
-
-	} catch (const std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
-		return;
-	}
-}
-
 int main(int argc, char **argv)
 {
 	if (argc == 2)
 	{
 		unsigned short port = strtol(argv[1], NULL, 10);
 
-		Daemon daemon(INADDR_ANY, port);
-
-		daemon.startThreads();
+		startNode(port);
 	} else
 	{
-		std::cout << "Usage: " << argv[0] << " <port>\n";
+		std::cout << "Usage: " << argv[0] << " <host port>\n";
 		return 0;
 	}
 
